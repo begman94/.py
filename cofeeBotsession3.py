1747e143128ecbe645a664d70fd3a61652defe55
begman94 <giacoman117@gmail.com>
2022-05-21 00:07:22 -0600

    2nd edit session 4
    added if conditionals based on given variables printing diferent results based on variable values

--- cofeeBotsession3.py
+++ cofeeBotsession3.py
@@ -1,18 +1,26 @@
 #Let's play with some numbers
 
-Welcome = input("hello welcome to the coffe shop are you ready to order?\n")
+from unicodedata import name
 
-Name = input("cool, what is your name?\n")
+
+Welcome = input("hello welcome to the coffe shop are you ready to order?\n\n")
+
+Name = input("cool, what is your name?\n\n")
+
+if name == "Ben":
+    print("you are not welcome here sr please get out of here")
+else:
+    print("Wecomen " + Name + " thank you for coming\n")
 
 menu = "black coffee, espresso, latte, cappucino"
+ 
+order = input("please, " + Name + ", tell me what you would like to dink today \n\nThis is our menu" + menu + ", please let me know your order \n\n:D \n\n")
 
-order = input("cool, " + Name + ", this is our menu, " + menu + ", please let me know your order \n:D \n")
-
-quantity = input("great, how many " + order + "'s do you want?\n")
+quantity = input("great, how many " + order + "'s do you want?\n\n")
 
 price = 10
 
-print("Great, " + quantity + " " + order + "'s will be served in no time, BRB! ;D")
+print("Great, " + quantity + " " + order + "'s will be served in no time, BRB! \n\n;D\n\n")
 
 bill = price * int(quantity)
 
