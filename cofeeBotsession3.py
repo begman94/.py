0d06b7c900b8254e3186a1ab25fe46670b8814d3
begman94 <giacoman117@gmail.com>
2022-05-21 23:50:52 -0600

    zzz
    zzz

--- cofeeBotsession3.py
+++ cofeeBotsession3.py
@@ -1,11 +1,15 @@
+from tkinter.messagebox import YES
+from unicodedata import name
+
+
 Welcome = input("hello welcome to the coffe shop are you ready to order?\n")
 
 Name = input("cool, what is your name?\n")
 
-if Name == "Ben":
-    evil_status = input("are you a evil Ben?\n")
+if Name == "Ben" or Name == "Pat" or Name == "Cullo":
+    evil_status = input("are you a evil " + Name)
     if evil_status == "yes":
-        print("you are not welcome here sr please get out of here")
+        print("you are not welcome here sr please get out of here " + Name)
         exit()
     else:
         print("Wecome, so you are one of that good Bens\n")
@@ -34,9 +38,19 @@ print("cost $" + str(price))
 
 quantity = input("How many " + order + "'s do you want?\n")
 
-print("Great, " + quantity + " " + order + "'s will be served in no time, BRB!\n")
-
 bill = price * int(quantity)
 
 print(bill)
 print("the total will be $" + str(bill))
+
+confirmation = input("Sound good?")
+
+if confirmation == "yes":
+    print("Great, " + quantity + " " + order + "'s will be served in no time, BRB!\n")
+else:
+    print("Ok, tell me whats wrong")
+    
+
+
+
+
