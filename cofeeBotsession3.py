6223386e89e0421eb3216aea6ede235fc1c453e2
begman94 <giacoman117@gmail.com>
2022-05-21 22:46:02 -0600

    ififififif else
    ...

--- cofeeBotsession3.py
+++ cofeeBotsession3.py
@@ -1,33 +1,42 @@
-#Let's play with some numbers
+Welcome = input("hello welcome to the coffe shop are you ready to order?\n")
 
-from unicodedata import name
+Name = input("cool, what is your name?\n")
 
-
-Welcome = input("hello welcome to the coffe shop are you ready to order?\n\n")
-
-Name = input("cool, what is your name?\n\n")
-
-if name == "Ben":
-    print("you are not welcome here sr please get out of here")
+if Name == "Ben":
+    evil_status = input("are you a evil Ben?\n")
+    if evil_status == "yes":
+        print("you are not welcome here sr please get out of here")
+        exit()
+    else:
+        print("Wecome, so you are one of that good Bens\n")
 else:
     print("Wecomen " + Name + " thank you for coming\n")
 
 menu = "black coffee, espresso, latte, cappucino"
  
-order = input("please, " + Name + ", tell me what you would like to dink today \n\nThis is our menu " + menu + ", please let me know your order \n\n:D \n\n")
+order = input("please, " + Name + ", tell me what you would like to dink today \nThis is our menu " + menu + ", please let me know your order\n")
 
-quantity = input("great, how many " + order + "'s do you want?\n\n")
 
-price = 10
 
-print("Great, " + quantity + " " + order + "'s will be served in no time, BRB! \n\n;D\n\n")
+if order == "latte":
+    price = 15
+
+if order == "black coffee":
+    price = 3
+
+if order == "espresso":
+    price = 8
+
+if order == "cappucino":
+    price = 25
+
+print("cost $" + str(price))
+
+quantity = input("How many " + order + "'s do you want?\n")
+
+print("Great, " + quantity + " " + order + "'s will be served in no time, BRB!\n")
 
 bill = price * int(quantity)
 
 print(bill)
 print("the total will be $" + str(bill))
-
-if 2 > 3:
-    print("yep is true")
-else:
-    print("no is not true")
